43eda72609a079ab5433c285071e97327f960d63
furqonfatahillah <furqonfatahillah999@gmail.com>
2024-01-23 13:39:12 +0800

    Committing Tambah_barang_model.php with CRLF line endings

---
+++ app/models/Tambah_barang_model.php
@@ -0,0 +1,34 @@
+<?php
+
+class Tambah_barang_model{
+
+    private $db;
+    public function __construct(){
+        $this->db = new Database;
+    }
+
+    public function getKondisiBarang() {
+        $query = "SELECT * FROM mst_kondisi_barang ORDER BY kondisi_barang";
+        $this->db->query($query);
+        return $this->db->resultSet();
+    }
+
+    public function getSatuan() {
+        $query = "SELECT * FROM mst_satuan ORDER BY nama_satuan";
+        $this->db->query($query);
+        return $this->db->resultSet();
+    }
+    
+    public function getStatus() {
+        $query = "SELECT * FROM mst_status ORDER BY status";
+        $this->db->query($query);
+        return $this->db->resultSet();
+    }
+
+    public function getLokasiPenyimpanan() {
+        $query = "SELECT * FROM mst_lokasi_penyimpanan ORDER BY nama_lokasi_penyimpanan";
+        $this->db->query($query);
+        return $this->db->resultSet();
+    }
+    
+}
